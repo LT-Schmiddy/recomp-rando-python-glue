c56515f0e930464160911faa69997064481d8bab
LT-Schmiddy <schmidalex1996@gmail.com>
2026-03-06 22:46:21 -0500

    imports work correctly

--- module_files.py
+++ module_files.py
@@ -6,14 +6,16 @@ pyyaml_src_dir = root_dir.joinpath("./libs/pyyaml/lib/yaml")
 typing_extensions_src_dir = root_dir.joinpath("./libs/typing_extensions/src")
 websockets_src_dir = root_dir.joinpath("./libs/websockets/src/websockets")
 archipelago_src_dir = root_dir.joinpath("./libs/Archipelago")
+colorama_src_dir = root_dir.joinpath("./libs/colorama/colorama")
 local_src_dir = root_dir.joinpath("./src/python")
 
-nrm_archipelago_dir = Path("archipelago")
+nrm_archipelago_dir = Path(".")
 
 include_python_files: dict[Path, Path] = {
     # Including single files
     # Modules
     Path("schema.py"): root_dir.joinpath("./libs/schema/schema.py"),
+    Path("typing_extensions.py"): typing_extensions_src_dir.joinpath("typing_extensions.py"),
 
     # Archipelago Base Files
     nrm_archipelago_dir.joinpath("BaseClasses.py"): archipelago_src_dir.joinpath("./BaseClasses.py"),
@@ -26,13 +28,10 @@ include_python_files: dict[Path, Path] = {
     nrm_archipelago_dir.joinpath("Options.py"): archipelago_src_dir.joinpath("./Options.py"),
     nrm_archipelago_dir.joinpath("Utils.py"): archipelago_src_dir.joinpath("./Utils.py"),
     nrm_archipelago_dir.joinpath("settings.py"): archipelago_src_dir.joinpath("./settings.py"),
-    nrm_archipelago_dir.joinpath("requirements.txt"): archipelago_src_dir.joinpath("./requirements.txt"), # ?
     # Path("CustomGenerate.py"): root_dir.joinpath("./src/python/CustomGenerate.py"), # a custom `Generate.py` for custom json output
 
     # Archipelago Client Files
     nrm_archipelago_dir.joinpath("CommonClient.py"): archipelago_src_dir.joinpath("./CommonClient.py"),
-    nrm_archipelago_dir.joinpath("RecompClient.py"): local_src_dir.joinpath("./RecompClient.py"), # a custom `Client` that will be the main interface to Archipelago
-    Path("test.py"): local_src_dir.joinpath("./test.py"), # for testing purposes
     # Path("RecompClient.py"): local_src_dir.joinpath("./RecompClient.py"), # a custom `Client` that will be the main interface to Archipelago
 
     # Archipelago World Files
@@ -59,5 +58,6 @@ def populate_file_injection(injections: dict[Path, Path], inject_root: Path, sea
 
 # Including entire directories            
 populate_file_injection(include_python_files, Path("yaml"), pyyaml_src_dir)
-populate_file_injection(include_python_files, Path("typing_extensions"), typing_extensions_src_dir)
 populate_file_injection(include_python_files, Path("websockets"), websockets_src_dir)
+populate_file_injection(include_python_files, Path("colorama"), colorama_src_dir)
+populate_file_injection(include_python_files, Path("."), local_src_dir)
--- src/c/main.c
+++ src/c/main.c
@@ -7,8 +7,8 @@ void RandoGlue_Init() {
     rando_interp = REPY_RegisterSubinterpreter(); 
     
     REPY_PushInterpreter(rando_interp);
-    rando_globals = REPY_CreateDict(0);
     REPY_AddNrmToSysPath();
+    rando_globals = REPY_CreateDict(0);
     REPY_PopInterpreter();
 
     recomp_printf("Subinterpreter %s Initialized\n", "rando_interp");
@@ -18,15 +18,9 @@ void RandoGlue_Init() {
 
     REPY_FN_EXEC_CACHE(
         example1, 
-        // "import platform\n"
-        // "print(f'This is example Python code running on {platform.system()}')"
-        // "__name__ = ''\n"
-        "from .test import test_func\n"
+        "from rando_test import test_func\n"
         "test_func()\n"
-        // "from .CommonClient import run_as_textclient\n"
-        // "from RecompClient import test\n"
-        // "from .archipelago.CommonClient import run_as_textclient\n"
-        // "run_as_textclient()"
+        "from CommonClient import run_as_textclient\n"
     );
     
     REPY_FN_CLEANUP;
